2cdfc2dc75186453421ba99ba7047724712e2195
Obelisk <anusorn.b@gmail.com>
2026-09-14 17:04:16 +0700

    Intro Cutscene

--- Assets/Scripts/CutsceneCrosshairController.cs
+++ Assets/Scripts/CutsceneCrosshairController.cs
@@ -3,8 +3,8 @@ using UnityEngine.Playables;
 
 public class CutsceneController : MonoBehaviour
 {
-    [SerializeField] private PlayableDirector director;
-    [SerializeField] private GameObject canvas;
+    [SerializeField] PlayableDirector director;
+    [SerializeField] GameObject canvas;
 
     private void Awake()
     {
@@ -27,15 +27,8 @@ public class CutsceneController : MonoBehaviour
         }
     }
 
-    /*public void PlayCutscene()
-    {
-        if (canvas != null) canvas.SetActive(false);
-        director.Play();
-    }*/
-
     private void OnCutsceneFinished(PlayableDirector pd)
     {
-        // เมื่อ Timeline จบ ให้เปิด UI Canvas กลับมา
         if (canvas != null)
         {
             canvas.SetActive(true);
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,19 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    float speed = 1f;
-    Rigidbody rb;
+    float speed = 2f;
+    //Rigidbody rb;
+    NavMeshAgent agent;
+    Animator anim;
+
+    [SerializeField]
+    List<Transform> waypoints = new List<Transform>();
+    [SerializeField]
+    float waitTimeAtPoint = 3f;
+
+    int currentWaypointIndex = 0;
+    bool isWaiting = false;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        if (waypoints == null || waypoints.Count == 0) return;
+
+        GoToCurrentWaypoint();
+    }
+
+    void Update()
+    {
+        if (waypoints.Count == 0 || isWaiting) return;
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            anim.SetTrigger("Stop");
+            agent.speed = 0;
+            StartCoroutine(WaitAtWaypoint());
+        }
+    }
+
+    void GoToCurrentWaypoint()
+    {
+        if (waypoints.Count == 0) return;
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    void SelectNextWayPoint()
+    {
+        currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Count;
+    }
+
+    IEnumerator WaitAtWaypoint()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTimeAtPoint);
+        SelectNextWayPoint();
+        anim.SetTrigger("Walk");
+        agent.speed = speed;
+        GoToCurrentWaypoint();
+        isWaiting = false;
     }
 
     void FixedUpdate()
     {
-        Vector3 forwardMove = transform.forward * speed;
-        rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
+        //Vector3 forwardMove = transform.forward * speed;
+        //rb.linearVelocity = new Vector3(forwardMove.x, rb.linearVelocity.y, forwardMove.z);
     }
 
 }
